d9e5e5b4b215d776ba7e3d32051c3f986d9cbbf5
Ephraim Gregor <chaosexanima@gmail.com>
2013-01-09 21:49:23 -0500

    Added Google libraries and updated documents to have license.

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ Google Drive WP Plugin
 ======================
 
 A plugin created for Wordpress to add a shortcode to display files and folders from Google Drive.
+
+Copyright 2013  Ephraim Gregor  (email : ephraim@ephraimgregor.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
--- met-drive.php
+++ met-drive.php
@@ -6,5 +6,24 @@ Description: A plugin to authenticate with Google Drive in order to display file
 Author: Ephraim Gregor
 Version: 1.0
 Author URI: http://ephraimgregor.com/
+License: GPL3
 */
+
+/*  
+	Copyright 2013  Ephraim Gregor  (email : ephraim@ephraimgregor.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 ?>
